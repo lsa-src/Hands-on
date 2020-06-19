248c05cedeac61419f07a858613291caade037fa
lsa <ssuney@gmail.com>
2020-06-19 14:45:56 +0900

    init

---
+++ main.js
@@ -0,0 +1,9 @@
+// $(document).ready(function(){
+//     $('.header').height($(window).height())
+// })
+
+window.onresize = window.onload = function() {
+    document.getElementById('header').style.height = this.innerHeight+'px'
+}
+
+//vanilla javascript
